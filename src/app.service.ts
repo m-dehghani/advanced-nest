249ad4c07a2f0703b3f76fb3a166b6cdd0242508
Mohammad Dehghani <dehghany.m@gmail.com>
2025-02-09 00:15:16 +0000

    change the app service file to run tesets successfully

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello the others Worlds! please respect the others Worlds!';
+    return 'Hello World!';
   }
 
 }
